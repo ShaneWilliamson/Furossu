0b9bfd9c3aebb8fbfd12020c60577ee056d631bd
Shane Williamson <williamson.shane.a@gmail.com>
2019-10-24 17:45:56 -0600

    dedupe codes, add scoring mechanism to service

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -105,7 +105,9 @@ export const words = [
 	"gusts",
 ]
 
-const CODE_COUNT = 10;
+const CODE_COUNT = 15;
+const CORRECT_GUESS_VALUE = 200;
+const INCORRECT_GUESS_VALUE = -25;
 
 @Injectable({
 	providedIn: 'root'
@@ -114,18 +116,23 @@ export class GameService {
 	private _guesses: Guess[];
 	private _codes: Code[];
 	private _answerIdx: number;
+	private _isGameOver: boolean;
+
+	private _score: number;
 
 	constructor() {
 		this._guesses = [];
 		this._codes = [];
-	}
-
-	public getGuesses(): Guess[] {
-		return this._guesses;
+		this._score = 0;
+		this._isGameOver = false;
 	}
 
 	private randomIdx(maxIdx: number): number {
 		return Math.floor(Math.random() * (maxIdx + 1))
+	}
+
+	public getGuesses(): Guess[] {
+		return this._guesses;
 	}
 
 	public getCodes(): Code[] {
@@ -133,11 +140,32 @@ export class GameService {
 			return this._codes;
 		}
 		this._codes = [];
+		var selectedCodes = {};
 		for (var i = 0; i < CODE_COUNT; i++) {
 			// Add another word with index random between 0 and 99
-			this._codes.push({code: words[this.randomIdx(99)], guesses: 0});
+			let word = words[this.randomIdx(99)]
+			if (word in selectedCodes) {
+				i--;
+				continue;
+			}
+			selectedCodes[word] = true;
+			this._codes.push({ code: word, guesses: 0 });
 		}
 		this._answerIdx = this.randomIdx(CODE_COUNT - 1);
 		return this._codes;
+	}
+
+	public submitGuess(guess: Guess): void {
+		this._guesses.push(guess);
+		if (this._guesses[this._answerIdx].value === guess.value) {
+			this._isGameOver = true;
+			this.addScore(CORRECT_GUESS_VALUE);
+		} else {
+			this.addScore(INCORRECT_GUESS_VALUE);
+		}
+	}
+
+	private addScore(value: number): void {
+		this._score += value;
 	}
 }
