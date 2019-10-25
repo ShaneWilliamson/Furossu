f847db885d6adcae156620f463026ac3f2497764
Shane Williamson <williamson.shane.a@gmail.com>
2019-10-24 19:45:33 -0600

    totally wip, not working

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, Renderer2 } from '@angular/core';
+import { CmService, DEFAULT_SCRIPT } from '../cm.service';
+import * as cm from 'codemirror';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/addon/hint/javascript-hint.js';
+import 'codemirror/addon/lint/lint.js';
+import 'codemirror/addon/lint/javascript-lint.js';
+import 'codemirror/addon/edit/closebrackets.js';
+import 'codemirror/addon/edit/matchbrackets.js';
+import 'codemirror/addon/search/match-highlighter.js';
+(<any>window).JSHINT = require('jshint').JSHINT;
 
 @Component({
   selector: 'code-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent {
+export class HomePageComponent implements AfterViewInit {
+
+	constructor(private renderer: Renderer2, private cmService: CmService) { }
+	
+	ngAfterViewInit(): void {
+		this.cmService.editor = cm.fromTextArea(
+			this.renderer.selectRootElement('[codeEditor]'),
+			{
+				lineNumbers: true,
+				mode: "javascript",
+        gutters: ["CodeMirror-lint-markers"],
+        lint: true,
+        autoCloseBrackets: true,
+        matchBrackets: true,
+        highlightSelectionMatches: {showToken: /\w/},
+        viewportMargin: Infinity
+			}
+		);
+		this.cmService.editor.setValue(DEFAULT_SCRIPT);
+	}
 
 }
