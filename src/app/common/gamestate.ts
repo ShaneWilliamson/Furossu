511dc978fdcfc69995a4c3abbc7bbbb72a8843fd
Shane Williamson <williamson.shane.a@gmail.com>
2019-10-27 15:45:52 -0600

    Super wip right now, working on gameloop

---
+++ src/app/common/gamestate.ts
@@ -0,0 +1,7 @@
+import { Code } from './code';
+
+export class GameState {
+	codes: Code[];
+	self: any;
+	script: any;
+}
--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Guess } from './common/guess';
 import { Code } from './common/code';
 import { Observable, BehaviorSubject } from 'rxjs';
+import { SandboxService } from './sandbox/sandbox.service';
+import { GameState } from './common/gamestate';
 
 export const words = [
 	"koila",
@@ -121,16 +123,96 @@ export class GameService {
 
 	private answerIdx: number;
 	private isGameOver: boolean;
-
+	private gameState: GameState;
 	private score: number;
+	private speed: number = 10;
 
-	constructor() {
+	private intervalTimer: NodeJS.Timer;
+
+	// user code
+	private getGuess: any;
+
+	constructor(private sandboxService: SandboxService) {
 		this.resetState();
 		var placeholders = [];
-		for(var i = 0; i < CODE_COUNT; i++) {
-			placeholders.push({code: "*****", "guesses": 0});
+		for (var i = 0; i < CODE_COUNT; i++) {
+			placeholders.push({ code: "*****", "guesses": 0 });
 		}
 		this.codes$$.next(placeholders);
+	}
+
+	private gameLoop(): void {
+		if (!this.isGameOver) {
+			var gameState = this.backupGameState();
+			try {  // Wrapped in try-catch since getMove is user code
+				var move = this.getGuess();
+			} finally {
+				this.restoreGameState(gameState);
+			}
+
+			var result = this.doMove(move);
+			if (this.isGameOver) {  // so hacky
+				clearInterval(this.intervalTimer);
+				self.updateHighScore(self.game.score());
+			}
+		}
+	}
+
+	private doMove(guess: string): void {
+		try {
+			console.log("Guess: " + guess);
+		} catch (e) {
+			console.error(e);
+			this.isGameOver = true;
+		}
+		let codes = this.getCodesValue();
+		let answerCode = codes[this.answerIdx];
+		if (guess === answerCode.code) {
+			console.log("Correct guess!");
+			this.isGameOver = true;
+			answerCode.guesses += 1;
+			return;
+		}
+		for (var i = 0; i < this.gameState.codes.length; i++) {
+			if (guess === codes[i].code) {
+				codes[i].guesses += 1;
+			}
+		}
+	};
+
+	private setIntervalTimer(timer: NodeJS.Timer): void {
+		if (this.intervalTimer) {
+			clearInterval(this.intervalTimer);
+			this.intervalTimer = undefined;
+		}
+		this.intervalTimer = setInterval(this.gameLoop, 1000 / this.speed);
+	}
+
+	private start(): void {
+		if(this.isGameOver && this.gameState.script) {
+			this.drawGrid();
+			this.sandbox = new BattleshipGameSandbox({
+					getBoardWidth: function() { return this.boardWidth; },
+					getBoardHeight: function() { return this.boardHeight; },
+					getMoves: function() { return this.moves() },
+					getBoard: this.getBoard,
+					getShips: this.getShips
+			});
+			this.setGetMoveFunction(this.code);
+				this.over(false);
+				this.setIntervalTimer();
+		}
+};
+
+	private backupGameState(): GameState {
+		var gameState = this.gameState;
+		return Object.freeze(gameState);
+	}
+
+	private restoreGameState(gameState: GameState): void {
+		this.gameState.codes = gameState.codes;
+		this.gameState.script = gameState.script;
+		this.gameState.self = gameState.self;
 	}
 
 	private resetState(): void {
@@ -154,6 +236,10 @@ export class GameService {
 
 	public getCodes(): Observable<Code[]> {
 		return this.codes$;
+	}
+
+	public getCodesValue(): Code[] {
+		return this.codes$$.getValue();
 	}
 
 	public startGame(): void {
