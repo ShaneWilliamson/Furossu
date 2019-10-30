80916aeaaa9d966bca212c12729a274f37e6a2a3
Shane Williamson <swilliamson@vendasta.com>
2019-10-30 06:46:32 -0600

    add player name to scoreboard

--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -12,6 +12,7 @@ export class GameBoardComponent {
 	@Input('results') public guessResults$: Observable<GuessResult[]>;
 	@Input('score') public score: number;
 	@Input() isSinglePlayer: boolean = true;
+	@Input() title = "Game Board";
 
 	constructor(private gameService: GameService) {
 	}
